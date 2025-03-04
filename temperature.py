c33157e08469a936eaff0a784fa90c6bf429ee9d
ZacharyAF-marcellin <zachary.alcoberfeetham@marcellin.school.nz>
2025-03-04 15:13:22 +1300

    saved GUI changes

--- temperature.py
+++ temperature.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 
+title = tk.Label(root, text = "Temperature\nConverter")
+title.grid(row = 0, column = 0)
+
+number = tk.Entry(root)
+number.grid(row = 1, column = 0)
+
 root.mainloop()
