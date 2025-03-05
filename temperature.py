d4a38c6a120f34707992d101e581d10220ee3f47
ZacharyAF-marcellin <zachary.alcoberfeetham@marcellin.school.nz>
2025-03-05 13:06:19 +1300

    added code w/ widgets

--- temperature.py
+++ temperature.py
@@ -1,11 +1,29 @@
 import tkinter as tk 
+from tkinter import ttk
+
+def convert():
+
+    result_1.config(text = f"{number.get()}")
 
 root = tk.Tk()
+con_var = tk.IntVar()
 
-title = tk.Label(root, text = "Temperature\nConverter")
-title.grid(row = 0, column = 0)
+title = ttk.Label(root, text = "Temperature\nConverter")
+title.grid(row = 0, column = 0, columnspan = 2, padx = 10, pady = 10)
 
-number = tk.Entry(root)
-number.grid(row = 1, column = 0)
+number = ttk.Entry(root)
+number.grid(row = 1, column = 0, columnspan = 2)
+
+c_to_f_rb = ttk.Radiobutton(root, text = "C to F", variable = con_var, value = 1)
+c_to_f_rb.grid(row = 2, column = 0)
+
+f_to_c_rb = ttk.Radiobutton(root, text = "F to C", variable = con_var, value = 2)
+f_to_c_rb.grid(row = 2, column = 1)
+
+convert_b = ttk.Button(root, text = "Convert", command = convert)
+convert_b.grid(row = 3, column = 0, columnspan = 2 )
+
+result_1 = ttk.Label(root, text = "")
+result_1.grid(row = 4, column = 0, columnspan = 2)
 
 root.mainloop()
