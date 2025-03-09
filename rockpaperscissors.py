8031ea617bd7a928bb929e459071cd9a23f02799
ZacharyAF-marcellin <zachary.alcoberfeetham@marcellin.school.nz>
2025-03-10 10:45:16 +1300

    new added rpc

---
+++ rockpaperscissors.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Rock Paper Scissors")
+
+opp = ttk.Entry(root, textvariable =  ) 
+opp.grid(row = 3, column = 0,columnspan = 2)
+
+root.mainloop()
